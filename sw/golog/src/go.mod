9f9e6eb2e50992b8ed60a36e68f254e19d35839b
Roman Dvořák <romandvorak@mlab.cz>
2022-11-01 18:47:04 +0100

    uprava golang skriptu

---
+++ sw/golog/src/go.mod
@@ -0,0 +1,9 @@
+module GOLOG
+
+go 1.13
+
+require (
+	fyne.io/fyne/v2 v2.2.3
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	github.com/wissance/stringFormatter v1.0.3
+)
--- sw/golog/src/main.go
+++ sw/golog/src/main.go
@@ -9,8 +9,19 @@ import (
 	  "os"
       "time"
       "github.com/wissance/stringFormatter"
-      //"strings"
+      	"fyne.io/fyne/v2/app"
+      	"fyne.io/fyne/v2/widget"
+        "bytes"
+      //"net"
+        "strings"
 )
+
+
+
+func window() {
+
+}
+
 
 func main() {
 
@@ -18,17 +29,14 @@ func main() {
 		logf := flag.String("log", "~/log_{datetime}.txt", "Output log filename, magic strings {datetime}")
 		port := flag.String("port", "/dev/ttyUSB0", "Input port")
 		port_baud := flag.Int("baud", 115200, "Input port baudrate")
+		gui_en := flag.Bool("gui", false, "Eneable gui")
+		//udp_port := flag.Int("upd", 3000, "UDP socket port")
 		flag.Parse()
 
 
         logfile := stringFormatter.FormatComplex(*logf, map[string]interface{} {
-            "datetime": time.Now().UTC().Format(time.RFC3339),
+            "datetime": strings.Replace(time.Now().UTC().Format(time.RFC3339), ":", "-", -1),
         })
-
-        // if strings.HasPrefix(logfile, "~/") {
-        //     uhd, _ := os.UserHomeDir()
-        //     logfile = filepath.Join(uhd, logfile[2:])
-        // }
 
         logfile, err :=filepath.Abs(logfile)
 
@@ -40,7 +48,8 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		//deferfile.Close()
+
+        file.Write([]byte(time.Now().UTC().Format(time.RFC3339) + "," +  "$LOGGER,golog,0.0,"+ "no-vcs" + "," + time.Now().UTC().Format(time.RFC3339) + "\n\r"))
 
 		c := &serial.Config{Name: *port, Baud: *port_baud}
 		s, err := serial.OpenPort(c)
@@ -48,14 +57,43 @@ func main() {
 			log.Fatal(err)
 		}
 
-		for true {
-			buf := make([]byte, 255)
-			n, err := s.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Print(time.Now().UTC().Format(time.RFC3339), string(buf[:n]))
-			file.Write([]byte("  empty "))
-			//file.Write(append([]byte(time.Now().UTC().Format(time.RFC3339))[:], buf[:n]))
-		}
+
+
+        a := app.New()
+        w := a.NewWindow("LABDOS data logger")
+        if(*gui_en == true) {
+            	w.SetContent(widget.NewLabel("LABDOS detector"))
+        }
+
+        go func() {
+            for true {
+                var content []byte
+        		for true {
+        			buf := make([]byte, 2048)
+        			_, err := s.Read(buf)
+        			if err != nil {
+        				log.Fatal(err)
+        			}
+                    if bytes.Contains(buf, []byte("\r")){
+                        //log.Print("KONEC EOL", n)
+                        break
+                    }
+                    content = append(content, buf...)
+
+                }
+    			fmt.Print(time.Now().UTC().Format(time.RFC3339), ",", string(content), "\n\r")
+    			file.Write( []byte(time.Now().UTC().Format(time.RFC3339) + "," + string(content) ))
+
+            }
+    	}()
+
+
+        if(*gui_en == true) {
+            	w.ShowAndRun()
+        } else {
+            for true {time.Sleep(1 * time.Second)}
+        }
+
+
+
 }
