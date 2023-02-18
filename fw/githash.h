a00af5631715c6c86ff9a23e9fd5d5ab148bc031
kakl <kakl@users.noreply.github.com>
2023-02-18 11:35:47 +0000

    new version handling, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "0c7346a5f8c8009998425ef7d90ed96681a10d10,user_build";
+String githash = "509d0531cecb5038014d1721b243632366b83475,user_build";
