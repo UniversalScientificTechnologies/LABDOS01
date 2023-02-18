511567cc35512de1829ca853307f18ba9726f3c6
roman-dvorak <roman-dvorak@users.noreply.github.com>
2023-02-18 12:29:45 +0000

    Update LABDOS.ino, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a87d9bbc82280b5192c1e5cc24f3ba251d929d88,user_build";
+String githash = "344e8937a7512e32dd07e8490ddcb5bdb378bf91,user_build";
