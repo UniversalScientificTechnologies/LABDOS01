e33e9010902a9942cd4ddb2b09ae4afe61eb2125
kakl <kakl@users.noreply.github.com>
2023-02-18 11:11:47 +0000

    source cleanup, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "ea397c00495abdf41f5b2061e0b3e20c6cc935f9,user_build";
+String githash = "e11c71b6d730cc8b83b4b5e2a5ee8ae069f42d64,user_build";
