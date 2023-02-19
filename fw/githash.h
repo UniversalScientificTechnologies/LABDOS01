7051204782d57b58ce521d7bde3addc1ff75f5a0
kakl <kakl@users.noreply.github.com>
2023-02-19 20:56:00 +0000

    RTC library was removed, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "193dd9f68f12b3f5ba2a623c5d9d5efcb8199376,user_build";
+String githash = "57e7100ac173615b6c2ea8be83464f865f546737,user_build";
