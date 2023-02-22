6e7c215d3cabfd29eca5e4f7b34e9f0814888025
kaklik <kaklik@users.noreply.github.com>
2023-02-22 20:08:03 +0000

    Update build_fw.yml, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "57695eac46190ef1045d0c73b26b5d1db05e9cfe,user_build";
+String githash = "ace04d02d5078e9df45d685c9417edcc37d756fe,user_build";
