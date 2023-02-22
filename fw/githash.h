33d93b35133e4ee6293d69782dd2fc254900644c
kakl <kakl@users.noreply.github.com>
2023-02-22 21:25:04 +0000

    comments with calculation of integration time, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "ace04d02d5078e9df45d685c9417edcc37d756fe,user_build";
+String githash = "196071f25c75e7832e27df829dbc7a472c803f03,user_build";
