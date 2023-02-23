4327fc9c75f0f63fe3f9224a7e377dd73766fcc3
roman-dvorak <roman-dvorak@users.noreply.github.com>
2023-02-23 18:32:56 +0000

    Update build_fw.yml, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,2 +1,2 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c33c9ec6608455e17d9c2b9020d78a9ade927ca5,user_build";
+String githash = "7942bfe2940a463ea317e19d36148bf9af52bb68,user_build";
