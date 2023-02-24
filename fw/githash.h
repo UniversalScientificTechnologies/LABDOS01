167965309c313e31df08bfc92669da8f2c904e5a
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-24 22:02:23 +0000

    another.., extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "bc393c9da575c21e97d314baaaa1a5de2ccfc7e7,user_build";
+String githash = "bc39a689bbf9bd4a6fb293d2919411e15be0a6c9,user_build";
 #define RELEASE 0
 
 #define BUILD 0
