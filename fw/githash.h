97a64bc910ce3bc4f5a8cba25707073e93bd4e34
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-24 16:23:19 +0000

    increase minor, extended build

--- fw/githash.h
+++ fw/githash.h
@@ -1,6 +1,6 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "123036324e9823819a3eddfd9d7dbb2bc13f7208,user_build";
-#define RELEASE 6
+String githash = "d792b413da20665c695b368322ff95ac8d4486b8,user_build";
+#define RELEASE 0
 
 #define BUILD 0
 #define BUILDTYPE User
