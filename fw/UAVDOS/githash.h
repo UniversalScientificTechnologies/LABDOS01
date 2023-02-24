0b21640de57d100dd6068a8c5eb748c7df32f6b9
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-24 23:46:14 +0000

    Merge branch 'LABDOS01A' of github.com:UniversalScientificTechnologies/LABDOS01 into LABDOS01A, extended build

--- fw/UAVDOS/githash.h
+++ fw/UAVDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "6d60f497dd33bde5c4d390de9965932a40fb8789,User";
+String githash = "88d18c35fc09475b4ced21b0dfde36477df2d0f1,User";
 #define RELEASE 2
 #define BUILD 0
 #define BUILDTYPE User
