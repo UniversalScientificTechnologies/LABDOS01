44e5dd2417519160d46df1bcc925c53df0327aa5
Vít Hanousek <vithanousek@seznam.cz>
2023-02-24 23:34:25 +0100

    separate githash.h

--- fw/UAVDOS/githash.h
+++ fw/UAVDOS/githash.h
@@ -1,1 +1,6 @@
-../githash.h
+// This file is overwritten by github actions, do not update it manually
+String githash = "6464b2c311ff6955377e496f642663251ec5689e,user_build";
+#define RELEASE 0
+
+#define BUILD 0
+#define BUILDTYPE User
