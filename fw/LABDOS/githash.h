f11453c5df55a7869e22c9a543ecae67060e94ce
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-25 00:29:58 +0000

    add Build type to file name, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "618e34cbe6e3add8d6028d1a8f8a6ea03c077c28,User";
+String githash = "30046ac1d33f296c4d530ce1a661f7ec3049e3f5,User";
 #define RELEASE 2
 #define BUILD 0
 #define BUILDTYPE User
