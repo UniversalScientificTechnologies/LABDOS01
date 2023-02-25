094481e3907062d2e4f09bad9931430f86339ec6
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-25 00:35:52 +0000

    set UAVEDOS to build 10, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "30046ac1d33f296c4d530ce1a661f7ec3049e3f5,User";
+String githash = "22c36428e02befc3d286fae4cf06dcf4c61687bd,User";
 #define RELEASE 2
 #define BUILD 0
 #define BUILDTYPE User
