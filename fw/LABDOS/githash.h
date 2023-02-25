f187a6e2d5fa9ed348082a2d177d9e04c08396c3
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-25 01:15:45 +0000

    try to fix build with correct version strig, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "45f600db99095f8509bb791156e83b2d9f5f00de,User";
+String githash = "41c59d0fee8283e8167c63231ebbd69bf6dbf775,User";
 #define GHRELEASE 2
 #define GHBUILD 0
 #define GHBUILDTYPE User
