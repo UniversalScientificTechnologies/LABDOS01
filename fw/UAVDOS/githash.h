d13d356198ebaf4caf2d97aed5a2a5830ba72c4a
slimonslimon <slimonslimon@users.noreply.github.com>
2023-02-25 01:36:27 +0000

    fix LABDOS version string, extended build, extended build

--- fw/UAVDOS/githash.h
+++ fw/UAVDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "27a4bd2946f5f5bb41b307b60bb6e11ca4b3af6b,User";
+String githash = "6ea9e030aedea0f9bc2e52386b3bfc080d8c3b29,User";
 #define GHRELEASE 2
 #define GHBUILD 0
 #define GHBUILDTYPE User
