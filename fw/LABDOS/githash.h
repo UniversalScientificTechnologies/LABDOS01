1afe9110d2a91a997f2c57aa035486bf50ce3cad
kakl <kakl@users.noreply.github.com>
2023-03-18 13:25:32 +0000

    test with small balloon radon collector, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c92661acdd51ac8095a079ab44ea089d5385ede0,User";
+String githash = "3422796e2a2ab2dd22c1cf0a2d9d08d911611b0d,User";
 #define GHRELEASE 3
 #define GHBUILD 0
 #define GHBUILDTYPE User
