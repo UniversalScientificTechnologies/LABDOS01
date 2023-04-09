6b3a8a224ff0453e757da4104813f9d03c1312f3
roman-dvorak <roman-dvorak@users.noreply.github.com>
2023-04-09 18:16:03 +0000

    Update README.md, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c92661acdd51ac8095a079ab44ea089d5385ede0,User";
+String githash = "4f134b6a9c2645fd21489f2ea65bf4ee99cb9cfa,User";
 #define GHRELEASE 3
 #define GHBUILD 0
 #define GHBUILDTYPE User
