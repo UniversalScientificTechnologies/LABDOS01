bed8c913db49c88a24b8bc88c2497a247f90677e
kakl <kakl@users.noreply.github.com>
2023-11-24 21:47:44 +0000

    eternal voltage reference, extended build

--- fw/UAVDOS/githash.h
+++ fw/UAVDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "71cdf196883fa3ab648ae47327ef2137eb69420f,User";
+String githash = "0d9005f3f6e174bb970c736833f1234794725fb0,User";
 #define GHRELEASE 3
 #define GHBUILD 0
 #define GHBUILDTYPE User
