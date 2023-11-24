f39de2aaa99f11cd9f2fdfbc069f4e5d17758e95
kaklik <kaklik@users.noreply.github.com>
2023-11-24 22:15:40 +0000

    Update LABDOS.ino, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "0d9005f3f6e174bb970c736833f1234794725fb0,User";
+String githash = "b7b5d52f50649c658c81e64f7c1a3c5abdc06e3b,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
