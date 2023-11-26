a53f7203143f6eab1d080726ee4e5b557c0b7e68
Jakub Kákona <kaklik@mlab.cz>
2023-11-26 01:39:38 +0100

    Introduce serial link as parameter to enable logging from multiple LABDOSes.

--- sw/log_labdos.sh
+++ sw/log_labdos.sh
@@ -1,6 +1,6 @@
 echo "LABDOS01 data logger"
 echo "To exit, pres ctrl-a, ctrl-x"
-echo " "
+echo "usage example: ./log_labdos.sh /dev/ttyUSB0 "
 
 mkdir ~/labdos01/ >/dev/null 2>&1
-picocom -b 115200 -q /dev/ttyUSB0 | awk '{ print strftime("%s,")$0; fflush(); }' | tee ~/labdos01/LABDOS_$(date +"%Y%m%d_%H%M%S").log
+picocom -b 115200 -q $1 | awk '{ print strftime("%s,")$0; fflush(); }' | tee ~/labdos01/LABDOS_$(date +"%Y%m%d_%H%M%S").log
