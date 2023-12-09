9be240d363d108b7296a09529b6603e5293c299a
kaklik <kaklik@users.noreply.github.com>
2023-12-09 17:23:59 +0000

    Merge branch 'LABDOS01A' of github.com:UniversalScientificTechnologies/LABDOS01 into LABDOS01A, extended build, extended build

--- fw/UAVDOS/githash.h
+++ fw/UAVDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a4c140ec09c2aa0b8e2e1424d23d7d28babb49e6,User";
+String githash = "1311793d48915b81accec155353eb83dda536fee,User";
 #define GHRELEASE 3
 #define GHBUILD 0
 #define GHBUILDTYPE User
