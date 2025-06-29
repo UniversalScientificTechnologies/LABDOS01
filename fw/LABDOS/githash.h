4fb3d59216293a17a8f1f4801ba42fa14ccb0f71
kaklik <kaklik@users.noreply.github.com>
2025-06-29 14:52:00 +0000

    Update workflows., extended build, extended build, extended build, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a10f5c56bcb880a6f793430e3ea4d6619c45415a,User";
-#define GHRELEASE 2
+String githash = "b8b987257b597757c4fba2e771ff27426d5c2ce0,User";
+#define GHRELEASE 3
 #define GHBUILD 0
 #define GHBUILDTYPE User
