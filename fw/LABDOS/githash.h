dafcecff376dbf796fc07708ebe8835b330f2d86
kaklik <kaklik@users.noreply.github.com>
2026-02-14 23:48:18 +0000

    Update README.md, extended build

--- fw/LABDOS/githash.h
+++ fw/LABDOS/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a80ed4bb9fc55f0fe886fe8b30b65f14035959f9,User";
+String githash = "cbacfbdd65d43bef95dcc19c1a2bed90ff005a38,User";
 #define GHRELEASE 5
 #define GHBUILD 0
 #define GHBUILDTYPE User
