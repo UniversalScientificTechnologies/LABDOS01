aeaa1371cd4e902856b6b0998a0aa22259fae8fa
Roman Dvorak <romandvorak@mlab.cz>
2026-09-01 21:54:25 +0200

    log_labdos.sh: optional description suffix in log filename

--- sw/log_labdos.sh
+++ sw/log_labdos.sh
@@ -1,6 +1,12 @@
 echo "LABDOS01 data logger"
 echo "To exit, pres ctrl-a, ctrl-x"
-echo "usage example: ./log_labdos.sh /dev/ttyUSB0 "
+echo "usage example: ./log_labdos.sh /dev/ttyUSB0 [description]"
 
 mkdir ~/labdos01/ >/dev/null 2>&1
-picocom -b 115200 -q $1 | awk '{ print strftime("%s,")$0; fflush(); }' | tee ~/labdos01/LABDOS_$(date +"%Y%m%d_%H%M%S").log
+
+DESC=""
+if [ -n "$2" ]; then
+  DESC="_$2"
+fi
+
+picocom -b 115200 -q $1 | awk '{ print strftime("%s,")$0; fflush(); }' | tee ~/labdos01/LABDOS_$(date +"%Y%m%d_%H%M%S")${DESC}.log
